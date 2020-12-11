16288a95cb0f4edec70c38f3267c123405a49573
Runal <bandishtirunal@gmail.com>
2020-12-11 20:31:56 +0530

    Second Commit

---
+++ String Interpolation/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+}
