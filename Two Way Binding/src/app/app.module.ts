e7a1834a1ed2de67f7e9b460f268c906dedfe31e
Runal <bandishtirunal@gmail.com>
2020-12-12 21:49:19 +0530

    New Commit

---
+++ Two Way Binding/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent
+    
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Two Way Binding/src/app/server/server.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverId = 10;
+  serverStatus = 'offline';
+  serverName='TestServer';
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server Was Created';
+  }
+
+  onUpdateServerName(event : any){
+    this.serverName = event.target.value;
+    
+  }
+}
